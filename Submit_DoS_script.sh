f04b14d2091ed9e687665b82637b625d4aac5bc8
BeckySTaylor <85310401+BeckySTaylor@users.noreply.github.com>
2025-12-18 11:08:55 -0500

    Update Submit_DoS_script.sh

--- Submit_DoS_script.sh
+++ Submit_DoS_script.sh
@@ -1,3 +1,6 @@
+##Script to run the DoS calculator script (which does not need changing). Enter the path and names of the non-synonymous and synonymous VCF files, respectively.
+##The give the name of a file containing a comma-separated list of individuals to be included ('-p', example included as another file), the outgroup individuals for '-o', the name of the output file, and the minumum number for Pn + Dn. 
+
 ./dos_calculator.sh \
   -n ../Apr2025_96indv_Arctic_NM1_NALout_autosomes_Reindeer_5missing_nonSyn_biallelic.recode.vcf.gz \
   -s ../Apr2025_96indv_Arctic_NM1_NALout_autosomes_Reindeer_5missing_Syn_biallelic.recode.vcf.gz \
