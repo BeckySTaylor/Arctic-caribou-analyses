45ad9fb3984cd44c6c1af0d05290337e377b27b5
BeckySTaylor <85310401+BeckySTaylor@users.noreply.github.com>
2025-12-18 10:14:41 -0500

    Create Submit_DoS_script.sh

---
+++ Submit_DoS_script.sh
@@ -0,0 +1,7 @@
+./dos_calculator.sh \
+  -n ../Apr2025_96indv_Arctic_NM1_NALout_autosomes_Reindeer_5missing_nonSyn_biallelic.recode.vcf.gz \
+  -s ../Apr2025_96indv_Arctic_NM1_NALout_autosomes_Reindeer_5missing_Syn_biallelic.recode.vcf.gz \
+  -p All_Inds.txt \
+  -o 22832,27689,27694,45932,45933 \
+  -O dos_results_All_grouped_Nalout_5missing.tsv \
+  -m 4
